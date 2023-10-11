5ceea872121d96d3ab1c7b062b159e090599f1a9
Vlad Uta <Vlad.Uta@qubiz.local>
2023-10-11 15:18:33 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { TeamsComponent } from './teams/teams.component';
+import { IconsComponent } from './icons/icons.component';
+import { HomeComponent } from './home/home.component';
+import { CirclesComponent } from './home/circles/circles.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { LatestComponent } from './home/latest/latest.component';
+import { MissionComponent } from './home/mission/mission.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    TeamsComponent,
+    IconsComponent,
+    HomeComponent,
+    CirclesComponent,
+    HeroComponent,
+    LatestComponent,
+    MissionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { CirclesComponent } from './circles/circles.component';
+// import { HeroComponent } from './hero/hero.component';
+// import { LatestComponent } from './latest/latest.component';
+// import { MissionComponent } from './mission/mission.component';
+// import { HomeComponent } from './home.component';
+
+// @NgModule({
+//   declarations: [
+//     CirclesComponent,
+//     HeroComponent,
+//     LatestComponent,
+//     MissionComponent,
+//     HomeComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//   ],
+//   exports:[
+//     HomeComponent
+//   ],
+//   providers: [],
+//   bootstrap: [HomeComponent]
+// })
+// export class HomeModule { }
