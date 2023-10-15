d3f042ca2fc6d66d1b62a1b901a4a3fc2a96cfcc
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-10-15 18:57:32 +0300

    icons + 50 % pages

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -1,25 +1,42 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-
 export class APIService {
-    private URL = 'http://127.0.0.1:3000/api/v1';
+  private URL = 'http://127.0.0.1:3000/api/v1';
+  private results: number = 0;
 
-    constructor() {}
+  constructor() {}
 
-    async getAllIcons() {
-        const res = await fetch(`${this.URL}/icons`);
-        const data = await res.json();
-        
-        return data.data.icons;
-    }
+  async getAllIcons() {
+    const res = await fetch(`${this.URL}/icons`);
+    const data = await res.json();
 
-    async getLatestIcons(nr: number) {
-        const res = await fetch(`${this.URL}/icons?sort=-retired&limit=${nr}`);
-        const data = await res.json();
+    this.results = data.results;
 
-        return data.data.icons;
-    }
+    return data.data.icons;
+  }
+
+  async getLatestIcons(nr: number) {
+    const res = await fetch(`${this.URL}/icons?sort=-retired&limit=${nr}`);
+    const data = await res.json();
+
+    this.results = data.results;
+
+    return data.data.icons;
+  }
+
+  async getIconsWithQuery(query: string) {
+    const res = await fetch(`${this.URL}/icons?${query}`);
+    const data = await res.json();
+
+    this.results = data.results;
+
+    return data.data.icons;
+  }
+
+  getResults() {
+    return this.results;
+  }
 }
--- src/app/icons/icon-form/icon-form.component.ts
+++ src/app/icons/icon-form/icon-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FilterService } from 'src/app/_services/formOptions.service';
 
@@ -14,6 +14,8 @@ export class IconFormComponent {
   trophyFilter: any[];
   queryForm: FormGroup;
 
+  @Output() newQueryEvent = new EventEmitter<any>();
+
   constructor(filterSvr: FilterService) {
     this.countryFilter = filterSvr.getCountryFilter();
     this.sortFilter = filterSvr.getSortFilter();
@@ -26,5 +28,52 @@ export class IconFormComponent {
       position: new FormControl(''),
       sort: new FormControl(''),
     });
+  }
+
+  ngOnInit() {
+    this.queryForm.setValue({
+      trophy: 'all',
+      country: 'all',
+      position: 'all',
+      sort: 'fantascore',
+    });
+
+    this.createQueryString({
+      trophy: 'all',
+      country: 'all',
+      position: 'all',
+      sort: 'fantascore',
+    });
+  }
+
+  queryIcons() {
+    const trophy = this.queryForm.controls['trophy'].value;
+    const country = this.queryForm.controls['country'].value;
+    const position = this.queryForm.controls['position'].value;
+    const sort = this.queryForm.controls['sort'].value;
+
+    const queryObj = {
+      trophy,
+      country,
+      position,
+      sort,
+    };
+
+    console.log(queryObj);
+    this.createQueryString(queryObj);
+  }
+
+  createQueryString(queryObj: any) {
+    let query = '';
+
+    let filterBy = [];
+    if (queryObj.trophy !== 'all') filterBy.push(queryObj.trophy);
+    if (queryObj.position !== 'all') filterBy.push(queryObj.position);
+
+    if (filterBy) query += `filterBy=${filterBy.join(',')}&`;
+    if (queryObj.country !== 'all') query += `country=${queryObj.country}&`;
+    query += `sortBy=${queryObj.sort}`;
+
+    this.newQueryEvent.emit({ query, queryObj });
   }
 }
